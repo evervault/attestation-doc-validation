440c4efd87b72cd588527eeacd00dce816584e46
David Nugent <david.nugent2425@gmail.com>
2022-11-03 14:11:47 +0000

    add clippy pedantic warnings and improve error handling (#2)

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,9 @@
 use aws_nitro_enclaves_cose::CoseSign1;
 use aws_nitro_enclaves_nsm_api::api::{AttestationDoc, Digest};
-use openssl::{stack::*, x509::*};
+use openssl::{
+    stack::Stack,
+    x509::{store, X509StoreContext, X509},
+};
 use std::collections::BTreeMap;
 
 /**
@@ -9,11 +12,21 @@ use std::collections::BTreeMap;
 
 static NITRO_ROOT_CA_BYTES: &[u8] = include_bytes!("nitro.pem");
 
+#[derive(Debug, PartialEq, Eq, Clone)]
 pub struct PCRs {
     pub pcr_0: String,
     pub pcr_1: String,
     pub pcr_2: String,
     pub pcr_8: String,
+}
+
+impl ToString for PCRs {
+    fn to_string(&self) -> String {
+        format!(
+            "PCR0: {},\nPCR1: {},\nPCR2: {},\nPCR8: {}",
+            self.pcr_0, self.pcr_1, self.pcr_2, self.pcr_8
+        )
+    }
 }
 
 use thiserror::Error;
@@ -35,8 +48,10 @@ where
     UntrustedCert,
     #[error("The COSE signature does not match the public key provided in the attestation doc")]
     InvalidCoseSignature,
-    #[error("The PCRs found were different to those that were expected")]
-    UnexpectedPCRs,
+    #[error("The PCRs found were different to those that were expected.\n\nExpected:\n{0}\n\nReceived:\n{1}")]
+    UnexpectedPCRs(String, String),
+    #[error("PCR{0} was missing in the attestation doc")]
+    MissingPCR(usize),
 }
 
 pub type Result<T> = std::result::Result<T, AttestationError>;
@@ -58,7 +73,7 @@ fn true_or_invalid(check: bool, err: AttestationError) -> Result<()> {
     }
 }
 
-/// https://github.com/aws/aws-nitro-enclaves-nsm-api/blob/main/docs/attestation_process.md
+/// Derived from [AWS attestation process](https://github.com/aws/aws-nitro-enclaves-nsm-api/blob/main/docs/attestation_process.md)
 pub fn validate_attestation_document_structure(
     attestation_document: &AttestationDoc,
 ) -> Result<()> {
@@ -82,25 +97,22 @@ pub fn validate_attestation_document_structure(
         && attestation_document
             .public_key
             .as_ref()
-            .map(|key| key.len() > 0 && key.len() <= 1024)
-            .unwrap_or(true) // these default to true if not present
+            .map_or(true, |key| key.len() > 0 && key.len() <= 1024) // these default to true if not present
         && attestation_document
             .nonce
             .as_ref()
-            .map(|nonce| nonce.len() > 0 && nonce.len() <= 512)
-            .unwrap_or(true)
+            .map_or(true, |nonce| nonce.len() > 0 && nonce.len() <= 512)
         && attestation_document
             .user_data
             .as_ref()
-            .map(|user_data| user_data.len() > 0 && user_data.len() <= 512)
-            .unwrap_or(true);
+            .map_or(true, |user_data| user_data.len() > 0 && user_data.len() <= 512);
     true_or_invalid(valid_structure_check, AttestationError::DocStructure)
 }
 
 fn vec_to_certificate_stack(certificates: Vec<X509>) -> Result<Stack<X509>> {
     let mut stack = Stack::new()?;
     for certificate in certificates {
-        stack.push(certificate)?
+        stack.push(certificate)?;
     }
     Ok(stack)
 }
@@ -120,7 +132,7 @@ fn verify_that_certificate_is_from_nitro_hypervisor(
             certificate_store.as_ref(),
             target.as_ref(),
             certificate_stack.as_ref(),
-            |context| context.verify_cert(),
+            openssl::x509::X509StoreContextRef::verify_cert,
         )?,
         AttestationError::UntrustedCert,
     )
@@ -139,26 +151,41 @@ fn validate_cose_signature(
     )
 }
 
-fn pcrs_match(attestation_doc: &AttestationDoc, expected_pcrs: PCRs) -> Result<()> {
+macro_rules! extract_pcr {
+    ($measurements:expr, $idx:literal) => {{
+        $measurements
+            .get(&$idx)
+            .ok_or(AttestationError::MissingPCR($idx))?
+            .to_string()
+    }};
+}
+
+fn pcrs_match(attestation_doc: &AttestationDoc, expected_pcrs: &PCRs) -> Result<()> {
     let encoded_measurements = attestation_doc
         .pcrs
         .iter()
         .map(|(&index, buf)| (index, hex::encode(&buf[..])))
         .collect::<BTreeMap<_, _>>();
 
-    let same_pcrs = Some(&expected_pcrs.pcr_0) == encoded_measurements.get(&0)
-        && Some(&expected_pcrs.pcr_1) == encoded_measurements.get(&1)
-        && Some(&expected_pcrs.pcr_2) == encoded_measurements.get(&2)
-        && Some(&expected_pcrs.pcr_8) == encoded_measurements.get(&8);
-    true_or_invalid(same_pcrs, AttestationError::UnexpectedPCRs)
+    let received_pcrs = PCRs {
+        pcr_0: extract_pcr!(encoded_measurements, 0),
+        pcr_1: extract_pcr!(encoded_measurements, 1),
+        pcr_2: extract_pcr!(encoded_measurements, 2),
+        pcr_8: extract_pcr!(encoded_measurements, 8),
+    };
+
+    let same_pcrs = expected_pcrs == &received_pcrs;
+    true_or_invalid(
+        same_pcrs,
+        AttestationError::UnexpectedPCRs(expected_pcrs.to_string(), received_pcrs.to_string()),
+    )
 }
 
 pub fn validate_attestation_doc(
-    attestation_doc_cose_sign_1_bytes: Vec<u8>,
-    expected_pcrs: PCRs,
+    attestation_doc_cose_sign_1_bytes: &[u8],
+    expected_pcrs: &PCRs,
 ) -> Result<AttestationDoc> {
-    let cose_sign_1_decoded: CoseSign1 =
-        serde_cbor::from_slice(&attestation_doc_cose_sign_1_bytes)?;
+    let cose_sign_1_decoded: CoseSign1 = serde_cbor::from_slice(attestation_doc_cose_sign_1_bytes)?;
     let attestation_doc: AttestationDoc = extract_attestation_doc(&cose_sign_1_decoded)?;
     validate_attestation_document_structure(&attestation_doc)?;
     let attestation_doc_signing_cert = X509::from_der(&attestation_doc.certificate)?;
@@ -195,7 +222,7 @@ mod attestation_tests {
             pcr_8: "8790eb3cce6c83d07e84b126dc61ca923333d6f66615c4a79157de48c5ab2418bdc60746ea7b7afbff03a1c6210201cb".to_string(),
         };
         let _attestation_doc =
-            validate_attestation_doc(sample_cose_sign_1_bytes, expected_pcrs).unwrap();
+            validate_attestation_doc(&sample_cose_sign_1_bytes, &expected_pcrs).unwrap();
     }
 
     #[test]
@@ -210,8 +237,10 @@ mod attestation_tests {
             pcr_2: "d8f114da658de5481funnexpecteeeeeeeeed60787522f705c92d7d96beed8e15e2aunnexpecteeeeeeeeed92e8dc469a".to_string(),
             pcr_8: "8790eb3cce6unnexpecteeeeeeeeed23333d6f66615c4a79157de48c5ab24unnexpecteeeeeeeeedafbff03a1c6210201cb".to_string(),
         };
-        let err = validate_attestation_doc(sample_cose_sign_1_bytes, expected_pcrs).unwrap_err();
-        assert!(matches!(err, AttestationError::UnexpectedPCRs));
+        let err = validate_attestation_doc(&sample_cose_sign_1_bytes, &expected_pcrs).unwrap_err();
+        assert!(
+            matches!(err, AttestationError::UnexpectedPCRs(expected_string, _) if expected_string == expected_pcrs.to_string())
+        );
     }
 
     #[test]
@@ -227,7 +256,7 @@ mod attestation_tests {
             pcr_2: "000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000".to_string(),
             pcr_8: "000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000".to_string(),
         };
-        let err = validate_attestation_doc(sample_cose_sign_1_bytes, expected_pcrs).unwrap_err();
+        let err = validate_attestation_doc(&sample_cose_sign_1_bytes, &expected_pcrs).unwrap_err();
         assert!(matches!(err, AttestationError::UntrustedCert));
     }
 }
