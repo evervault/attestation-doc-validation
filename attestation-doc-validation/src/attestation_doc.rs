78ca6cccb80f29937ddfc8adc50b08446872e390
Liam <liam@evervault.com>
2023-01-18 15:35:54 +0000

    update tests with valid attestation docs and certs

--- attestation-doc-validation/src/attestation_doc.rs
+++ attestation-doc-validation/src/attestation_doc.rs
@@ -86,11 +86,10 @@ impl PCRProvider for PCRs {
     }
 }
 
-
 ///
-/// 
+///
 /// # Errors
-/// 
+///
 /// Returns an error if any of the expected PCRs are missing from the attestation document, or if the expected PCRs don't match the values embedded in the doc
 pub fn validate_expected_pcrs<T: PCRProvider>(
     attestation_doc: &AttestationDoc,
--- attestation-doc-validation/src/lib.rs
+++ attestation-doc-validation/src/lib.rs
@@ -16,9 +16,9 @@ fn true_or_invalid<E: Into<error::AttestError>>(check: bool, err: E) -> Result<(
 }
 
 /// Parses a slice of bytes into an X509. First attempts to parse PEM, but falls back to DER
-/// 
+///
 /// # Errors
-/// 
+///
 /// If both PEM and DER decoding of the certificate fail
 pub fn parse_cert(given_cert: &[u8]) -> error::AttestResult<X509> {
     let parsed_cert =
@@ -34,7 +34,7 @@ pub fn parse_cert(given_cert: &[u8]) -> error::AttestResult<X509> {
 /// - Validating that the PCRs of the attestation doc are as expected
 ///
 /// # Errors
-/// 
+///
 /// Will return an error if:
 /// - The cose1 encoded attestation doc fails to parse, or its signature is invalid
 /// - The attestation document is not signed by the nitro cert chain
@@ -185,7 +185,7 @@ mod test {
     #[test]
     fn validate_valid_attestation_doc_in_cert() {
         let sample_cert_bytes = std::fs::read(std::path::Path::new(
-            "./test-files/pem-sample-cert-containing-attestation-doc-16-11-22-10-55",
+            "./test-files/debug-mode-cert-containing-attestation-doc-18-1-23.pem",
         ))
         .unwrap();
         let expected_pcrs = PCRs {
@@ -196,15 +196,36 @@ mod test {
         };
         let cert = parse_cert(&sample_cert_bytes).unwrap();
         let maybe_ad = validate_attestation_doc_in_cert(&cert);
+        println!("{maybe_ad:?}");
         assert!(maybe_ad.is_ok());
         let is_valid = validate_expected_pcrs(&maybe_ad.unwrap(), &expected_pcrs).is_ok();
         assert!(is_valid);
     }
 
     #[test]
+    fn validate_valid_attestation_doc_in_non_debug_mode_with_correct_pcrs() {
+        let sample_cert_bytes = std::fs::read(std::path::Path::new(
+            "./test-files/non-debug-cert-containing-attestation-document-18-1-23.pem",
+        ))
+        .unwrap();
+        let expected_pcrs = PCRs {
+          pcr_0: "d265a83faa7b4fa0d73b82b6d06253e894445922937d0ee5a74fe4891da9817611a71e71b98e5329232902de3cf419af".to_string(),
+          pcr_1: "bcdf05fefccaa8e55bf2c8d6dee9e79bbff31e34bf28a99aa19e6b29c37ee80b214a414b7607236edf26fcb78654e63f".to_string(),
+          pcr_2: "e4f634d24ad83b7f6e49fe283bafdb220f6252fb4a278a858e19d15b7cd0f263b9168ed0bacefb4444266df9f9a77f24".to_string(),
+          pcr_8: "97c5395a83c0d6a04d53ff962663c714c178c24500bf97f78456ed3721d922cf3f940614da4bb90107c439bc4a1443ca".to_string(),
+      };
+        let cert = parse_cert(&sample_cert_bytes).unwrap();
+        let maybe_ad = validate_attestation_doc_in_cert(&cert);
+        assert!(maybe_ad.is_ok());
+        let ad = maybe_ad.unwrap();
+        let pcrs_match = validate_expected_pcrs(&ad, &expected_pcrs).is_ok();
+        assert!(pcrs_match);
+    }
+
+    #[test]
     fn validate_valid_attestation_doc_in_cert_incorrect_pcrs() {
         let sample_cert_bytes = std::fs::read(std::path::Path::new(
-            "./test-files/pem-sample-cert-containing-attestation-doc-16-11-22-10-55",
+            "./test-files/non-debug-cert-containing-attestation-document-18-1-23.pem",
         ))
         .unwrap();
         let expected_pcrs = PCRs {
@@ -227,14 +248,14 @@ mod test {
     #[test]
     fn validate_valid_attestation_doc_in_cert_der_encoding() {
         let sample_cert_bytes = std::fs::read(std::path::Path::new(
-            "./test-files/der-sample-cert-containing-attestation-doc-16-11-22-10-55",
+            "./test-files/non-debug-cert-containing-attestation-document-18-1-23-der.crt",
         ))
         .unwrap();
         let expected_pcrs = PCRs {
-          pcr_0: "000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000".to_string(),
-          pcr_1: "000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000".to_string(),
-          pcr_2: "000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000".to_string(),
-          pcr_8: "000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000".to_string(),
+          pcr_0: "d265a83faa7b4fa0d73b82b6d06253e894445922937d0ee5a74fe4891da9817611a71e71b98e5329232902de3cf419af".to_string(),
+          pcr_1: "bcdf05fefccaa8e55bf2c8d6dee9e79bbff31e34bf28a99aa19e6b29c37ee80b214a414b7607236edf26fcb78654e63f".to_string(),
+          pcr_2: "e4f634d24ad83b7f6e49fe283bafdb220f6252fb4a278a858e19d15b7cd0f263b9168ed0bacefb4444266df9f9a77f24".to_string(),
+          pcr_8: "97c5395a83c0d6a04d53ff962663c714c178c24500bf97f78456ed3721d922cf3f940614da4bb90107c439bc4a1443ca".to_string(),
       };
         let cert = parse_cert(&sample_cert_bytes).unwrap();
         let maybe_ad = validate_attestation_doc_in_cert(&cert);
