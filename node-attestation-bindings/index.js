332ac5b8aab09a34155cad88c275229ea2bd0578
Liam <liam@evervault.com>
2023-02-08 14:54:36 +0000

    update workflow with new package name, update index.js to find native node bindings with correct name

--- node-attestation-bindings/index.js
+++ node-attestation-bindings/index.js
@@ -26,24 +26,24 @@ switch (platform) {
   case 'android':
     switch (arch) {
       case 'arm64':
-        localFileExisted = existsSync(join(__dirname, 'attestation-doc-validation.android-arm64.node'))
+        localFileExisted = existsSync(join(__dirname, 'evervault-attestation-bindings.android-arm64.node'))
         try {
           if (localFileExisted) {
-            nativeBinding = require('./attestation-doc-validation.android-arm64.node')
+            nativeBinding = require('./evervault-attestation-bindings.android-arm64.node')
           } else {
-            nativeBinding = require('attestation-doc-validation-android-arm64')
+            nativeBinding = require('evervault-attestation-bindings-android-arm64')
           }
         } catch (e) {
           loadError = e
         }
         break
       case 'arm':
-        localFileExisted = existsSync(join(__dirname, 'attestation-doc-validation.android-arm-eabi.node'))
+        localFileExisted = existsSync(join(__dirname, 'evervault-attestation-bindings.android-arm-eabi.node'))
         try {
           if (localFileExisted) {
-            nativeBinding = require('./attestation-doc-validation.android-arm-eabi.node')
+            nativeBinding = require('./evervault-attestation-bindings.android-arm-eabi.node')
           } else {
-            nativeBinding = require('attestation-doc-validation-android-arm-eabi')
+            nativeBinding = require('evervault-attestation-bindings-android-arm-eabi')
           }
         } catch (e) {
           loadError = e
@@ -57,13 +57,13 @@ switch (platform) {
     switch (arch) {
       case 'x64':
         localFileExisted = existsSync(
-          join(__dirname, 'attestation-doc-validation.win32-x64-msvc.node')
+          join(__dirname, 'evervault-attestation-bindings.win32-x64-msvc.node')
         )
         try {
           if (localFileExisted) {
-            nativeBinding = require('./attestation-doc-validation.win32-x64-msvc.node')
+            nativeBinding = require('./evervault-attestation-bindings.win32-x64-msvc.node')
           } else {
-            nativeBinding = require('attestation-doc-validation-win32-x64-msvc')
+            nativeBinding = require('evervault-attestation-bindings-win32-x64-msvc')
           }
         } catch (e) {
           loadError = e
@@ -71,13 +71,13 @@ switch (platform) {
         break
       case 'ia32':
         localFileExisted = existsSync(
-          join(__dirname, 'attestation-doc-validation.win32-ia32-msvc.node')
+          join(__dirname, 'evervault-attestation-bindings.win32-ia32-msvc.node')
         )
         try {
           if (localFileExisted) {
-            nativeBinding = require('./attestation-doc-validation.win32-ia32-msvc.node')
+            nativeBinding = require('./evervault-attestation-bindings.win32-ia32-msvc.node')
           } else {
-            nativeBinding = require('attestation-doc-validation-win32-ia32-msvc')
+            nativeBinding = require('evervault-attestation-bindings-win32-ia32-msvc')
           }
         } catch (e) {
           loadError = e
@@ -85,13 +85,13 @@ switch (platform) {
         break
       case 'arm64':
         localFileExisted = existsSync(
-          join(__dirname, 'attestation-doc-validation.win32-arm64-msvc.node')
+          join(__dirname, 'evervault-attestation-bindings.win32-arm64-msvc.node')
         )
         try {
           if (localFileExisted) {
-            nativeBinding = require('./attestation-doc-validation.win32-arm64-msvc.node')
+            nativeBinding = require('./evervault-attestation-bindings.win32-arm64-msvc.node')
           } else {
-            nativeBinding = require('attestation-doc-validation-win32-arm64-msvc')
+            nativeBinding = require('evervault-attestation-bindings-win32-arm64-msvc')
           }
         } catch (e) {
           loadError = e
@@ -102,23 +102,23 @@ switch (platform) {
     }
     break
   case 'darwin':
-    localFileExisted = existsSync(join(__dirname, 'attestation-doc-validation.darwin-universal.node'))
+    localFileExisted = existsSync(join(__dirname, 'evervault-attestation-bindings.darwin-universal.node'))
     try {
       if (localFileExisted) {
-        nativeBinding = require('./attestation-doc-validation.darwin-universal.node')
+        nativeBinding = require('./evervault-attestation-bindings.darwin-universal.node')
       } else {
-        nativeBinding = require('attestation-doc-validation-darwin-universal')
+        nativeBinding = require('evervault-attestation-bindings-darwin-universal')
       }
       break
     } catch {}
     switch (arch) {
       case 'x64':
-        localFileExisted = existsSync(join(__dirname, 'attestation-doc-validation.darwin-x64.node'))
+        localFileExisted = existsSync(join(__dirname, 'evervault-attestation-bindings.darwin-x64.node'))
         try {
           if (localFileExisted) {
-            nativeBinding = require('./attestation-doc-validation.darwin-x64.node')
+            nativeBinding = require('./evervault-attestation-bindings.darwin-x64.node')
           } else {
-            nativeBinding = require('attestation-doc-validation-darwin-x64')
+            nativeBinding = require('evervault-attestation-bindings-darwin-x64')
           }
         } catch (e) {
           loadError = e
@@ -126,13 +126,13 @@ switch (platform) {
         break
       case 'arm64':
         localFileExisted = existsSync(
-          join(__dirname, 'attestation-doc-validation.darwin-arm64.node')
+          join(__dirname, 'evervault-attestation-bindings.darwin-arm64.node')
         )
         try {
           if (localFileExisted) {
-            nativeBinding = require('./attestation-doc-validation.darwin-arm64.node')
+            nativeBinding = require('./evervault-attestation-bindings.darwin-arm64.node')
           } else {
-            nativeBinding = require('attestation-doc-validation-darwin-arm64')
+            nativeBinding = require('evervault-attestation-bindings-darwin-arm64')
           }
         } catch (e) {
           loadError = e
@@ -146,12 +146,12 @@ switch (platform) {
     if (arch !== 'x64') {
       throw new Error(`Unsupported architecture on FreeBSD: ${arch}`)
     }
-    localFileExisted = existsSync(join(__dirname, 'attestation-doc-validation.freebsd-x64.node'))
+    localFileExisted = existsSync(join(__dirname, 'evervault-attestation-bindings.freebsd-x64.node'))
     try {
       if (localFileExisted) {
-        nativeBinding = require('./attestation-doc-validation.freebsd-x64.node')
+        nativeBinding = require('./evervault-attestation-bindings.freebsd-x64.node')
       } else {
-        nativeBinding = require('attestation-doc-validation-freebsd-x64')
+        nativeBinding = require('evervault-attestation-bindings-freebsd-x64')
       }
     } catch (e) {
       loadError = e
@@ -162,26 +162,26 @@ switch (platform) {
       case 'x64':
         if (isMusl()) {
           localFileExisted = existsSync(
-            join(__dirname, 'attestation-doc-validation.linux-x64-musl.node')
+            join(__dirname, 'evervault-attestation-bindings.linux-x64-musl.node')
           )
           try {
             if (localFileExisted) {
-              nativeBinding = require('./attestation-doc-validation.linux-x64-musl.node')
+              nativeBinding = require('./evervault-attestation-bindings.linux-x64-musl.node')
             } else {
-              nativeBinding = require('attestation-doc-validation-linux-x64-musl')
+              nativeBinding = require('evervault-attestation-bindings-linux-x64-musl')
             }
           } catch (e) {
             loadError = e
           }
         } else {
           localFileExisted = existsSync(
-            join(__dirname, 'attestation-doc-validation.linux-x64-gnu.node')
+            join(__dirname, 'evervault-attestation-bindings.linux-x64-gnu.node')
           )
           try {
             if (localFileExisted) {
-              nativeBinding = require('./attestation-doc-validation.linux-x64-gnu.node')
+              nativeBinding = require('./evervault-attestation-bindings.linux-x64-gnu.node')
             } else {
-              nativeBinding = require('attestation-doc-validation-linux-x64-gnu')
+              nativeBinding = require('evervault-attestation-bindings-linux-x64-gnu')
             }
           } catch (e) {
             loadError = e
@@ -191,26 +191,26 @@ switch (platform) {
       case 'arm64':
         if (isMusl()) {
           localFileExisted = existsSync(
-            join(__dirname, 'attestation-doc-validation.linux-arm64-musl.node')
+            join(__dirname, 'evervault-attestation-bindings.linux-arm64-musl.node')
           )
           try {
             if (localFileExisted) {
-              nativeBinding = require('./attestation-doc-validation.linux-arm64-musl.node')
+              nativeBinding = require('./evervault-attestation-bindings.linux-arm64-musl.node')
             } else {
-              nativeBinding = require('attestation-doc-validation-linux-arm64-musl')
+              nativeBinding = require('evervault-attestation-bindings-linux-arm64-musl')
             }
           } catch (e) {
             loadError = e
           }
         } else {
           localFileExisted = existsSync(
-            join(__dirname, 'attestation-doc-validation.linux-arm64-gnu.node')
+            join(__dirname, 'evervault-attestation-bindings.linux-arm64-gnu.node')
           )
           try {
             if (localFileExisted) {
-              nativeBinding = require('./attestation-doc-validation.linux-arm64-gnu.node')
+              nativeBinding = require('./evervault-attestation-bindings.linux-arm64-gnu.node')
             } else {
-              nativeBinding = require('attestation-doc-validation-linux-arm64-gnu')
+              nativeBinding = require('evervault-attestation-bindings-linux-arm64-gnu')
             }
           } catch (e) {
             loadError = e
@@ -219,13 +219,13 @@ switch (platform) {
         break
       case 'arm':
         localFileExisted = existsSync(
-          join(__dirname, 'attestation-doc-validation.linux-arm-gnueabihf.node')
+          join(__dirname, 'evervault-attestation-bindings.linux-arm-gnueabihf.node')
         )
         try {
           if (localFileExisted) {
-            nativeBinding = require('./attestation-doc-validation.linux-arm-gnueabihf.node')
+            nativeBinding = require('./evervault-attestation-bindings.linux-arm-gnueabihf.node')
           } else {
-            nativeBinding = require('attestation-doc-validation-linux-arm-gnueabihf')
+            nativeBinding = require('evervault-attestation-bindings-linux-arm-gnueabihf')
           }
         } catch (e) {
           loadError = e
