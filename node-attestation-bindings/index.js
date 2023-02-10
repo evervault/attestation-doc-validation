d920d9ec616cbede6ee309105f0eb54e1a7ce97d
Liam <liam@evervault.com>
2023-02-10 17:47:56 +0000

    remove freebsd from index.js

--- node-attestation-bindings/index.js
+++ node-attestation-bindings/index.js
@@ -142,23 +142,6 @@ switch (platform) {
         throw new Error(`Unsupported architecture on macOS: ${arch}`);
     }
     break;
-  case "freebsd":
-    if (arch !== "x64") {
-      throw new Error(`Unsupported architecture on FreeBSD: ${arch}`);
-    }
-    localFileExisted = existsSync(
-      join(__dirname, "evervault-attestation-bindings.freebsd-x64.node")
-    );
-    try {
-      if (localFileExisted) {
-        nativeBinding = require("./evervault-attestation-bindings.freebsd-x64.node");
-      } else {
-        nativeBinding = require("evervault-attestation-bindings-freebsd-x64");
-      }
-    } catch (e) {
-      loadError = e;
-    }
-    break;
   case "linux":
     switch (arch) {
       case "x64":
