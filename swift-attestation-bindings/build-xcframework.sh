24b1b7953278ddfffef81c644772846d2d962111
Hannah Neary <92307259+hanneary@users.noreply.github.com>
2023-06-30 12:37:37 +0100

    Build fixes

--- swift-attestation-bindings/build-xcframework.sh
+++ swift-attestation-bindings/build-xcframework.sh
@@ -74,7 +74,7 @@ esac
 
 cargo_build () {
   TARGET=$1
-  env -i \
+  #env -i \
     "$CARGO" "${BUILD_ARGS[@]}" --target "$TARGET"
 }
 
@@ -88,7 +88,7 @@ CFLAGS_x86_64_apple_ios="-target x86_64-apple-ios" \
 cargo_build aarch64-apple-ios
 
 # M1 iOS simulator.
-CFLAGS_aarch64_apple_ios_sim="--target aarch64-apple-ios-sim" \
+CFLAGS_aarch64_apple_ios_sim="-target aarch64-apple-ios-sim" \
   cargo_build aarch64-apple-ios-sim
 
 # TODO: would it be useful to also include desktop builds here?
