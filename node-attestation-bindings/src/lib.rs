2b4e4a8ae93f4a5bd17854522c431e4cf76b42dc
Mark Doyle <markdoyle222@gmail.com>
2023-08-22 13:59:21 +0100

    add "attest_cage" method to node api

--- node-attestation-bindings/src/lib.rs
+++ node-attestation-bindings/src/lib.rs
@@ -1,5 +1,6 @@
 use attestation_doc_validation::attestation_doc::{validate_expected_pcrs, PCRProvider};
-use attestation_doc_validation::{parse_cert, validate_attestation_doc_in_cert};
+use attestation_doc_validation::{parse_cert, validate_attestation_doc_in_cert, validate_attestation_doc_against_cert};
+
 use napi::JsBuffer;
 use napi_derive::napi;
 
@@ -30,6 +31,8 @@ impl PCRProvider for NodePCRs {
   }
 }
 
+/// Deprecated legacy attestation method
+/// Supports attesting connections where the attestation doc is embedded in the certs SANs
 #[napi]
 fn attest_connection(cert: JsBuffer, expected_pcrs_list: Vec<NodePCRs>) -> bool {
   let cert_val = match cert.into_value() {
@@ -70,5 +73,58 @@ fn attest_connection(cert: JsBuffer, expected_pcrs_list: Vec<NodePCRs>) -> bool 
       eprintln!("Failed to validate that PCRs are as expected: {e}");
       false
     }
+  }
+}
+
+/// a client can call out to `<cage-url>/.well-known/attestation` to fetch the attestation doc from the cage
+/// The fetched attestation doc will have the public key of the domain's cert embedded inside it along with an expiry
+#[napi]
+fn attest_cage(cert: JsBuffer, expected_pcrs_list: Vec<NodePCRs>, attestation_doc: JsBuffer) -> bool {
+  let cert_val = match cert.into_value() {
+    Ok(cert_value) => cert_value,
+    Err(e) => {
+      eprintln!("Failed to access cert value passed from node to rust: {e}");
+      return false;
+    }
+  };
+
+  let parsed_cert = match parse_cert(cert_val.as_ref()) {
+    Ok(parsed_cert) => parsed_cert,
+    Err(e) => {
+      eprintln!("Failed to parse provided cert: {e}");
+      return false;
+    }
+  };
+
+  let attestation_doc_value = match attestation_doc.into_value() {
+    Ok(attestation_doc) => attestation_doc,
+    Err(e) => {
+      eprintln!("Failed to access attestation doc value passed from node to rust: {e}");
+      return false;
+    }
+  };
+
+  let validated_attestation_doc = match validate_attestation_doc_against_cert(&parsed_cert, attestation_doc_value.as_ref()) {
+    Ok(attestation_doc) => attestation_doc,
+    Err(e) => {
+      eprintln!("An error occur while validating the attestation doc against the Cage connection's cert: {e}");
+      return false;
+    }
+  };
+
+  let mut result = Ok(true);
+  for expected_pcrs in expected_pcrs_list {
+      match validate_expected_pcrs(&validated_attestation_doc, &expected_pcrs) {
+          Ok(_) => return true,
+          Err(err) => result = Err(err),
+      }
+  }
+  
+  match result {
+      Ok(_) => true,
+      Err(e) => {
+          eprintln!("Failed to validate that PCRs are as expected: {e}");
+          false
+      }
   }
 }
