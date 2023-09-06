e830dc3dc6a7ccabde5e1080b96534e41728c5e9
Lammert Westerhoff <westerhoff@gmail.com>
2023-09-06 03:58:08 -0500

    Swift (#56)
    * Add initial support for Swift
    
    * Build fixes
    
    * Add Package.swift
    
    * Fix plist
    
    * Add DS_Store to git ignore
    
    * Do actual attestion check for iOS
    
    * Remove Package.swift
    
    * Delete .DS_Store
    
    * fix after merge conflict
    
    ---------
    
    Co-authored-by: Hannah Neary <92307259+hanneary@users.noreply.github.com>
    Co-authored-by: Hannah Neary <hannah@evervault.com>

---
+++ swift-attestation-bindings/src/lib.rs
@@ -0,0 +1,74 @@
+use std::os::raw::c_int;
+use std::os::raw::c_char;
+use std::ffi::CStr;
+use std::slice;
+use attestation_doc_validation::attestation_doc::{validate_expected_pcrs, PCRProvider};
+use attestation_doc_validation::{parse_cert, validate_attestation_doc_in_cert};
+
+#[repr(C)]
+#[derive(Clone)]
+pub struct PCRs {
+  pcr_0: *const c_char,
+  pcr_1: *const c_char,
+  pcr_2: *const c_char,
+  pcr_8: *const c_char,
+}
+
+impl PCRProvider for &PCRs {
+  fn pcr_0(&self) -> Option<&str> {
+    unsafe { (*self).pcr_0.as_ref().and_then(|c_str| CStr::from_ptr(c_str).to_str().ok()) }
+  }
+
+  fn pcr_1(&self) -> Option<&str> {
+    unsafe { (*self).pcr_1.as_ref().and_then(|c_str| CStr::from_ptr(c_str).to_str().ok()) }
+  }
+
+  fn pcr_2(&self) -> Option<&str> {
+    unsafe { (*self).pcr_2.as_ref().and_then(|c_str| CStr::from_ptr(c_str).to_str().ok()) }
+  }
+
+  fn pcr_8(&self) -> Option<&str> {
+    unsafe { (*self).pcr_8.as_ref().and_then(|c_str| CStr::from_ptr(c_str).to_str().ok()) }
+  }
+}
+
+#[no_mangle]
+pub extern "C" fn attest_connection(cert: *const u8, cert_len: usize, expected_pcrs_list: *const PCRs, expected_pcrs_len: usize) -> bool {
+
+  assert!(!cert.is_null());
+
+  let cert_slice = unsafe { std::slice::from_raw_parts(cert, cert_len) };
+  let expected_pcrs_slice = unsafe { std::slice::from_raw_parts(expected_pcrs_list, expected_pcrs_len) };
+
+  let parsed_cert = match parse_cert(cert_slice) {
+    Ok(parsed_cert) => parsed_cert,
+    Err(e) => {
+        eprintln!("Failed to parse provided cert: {e}");
+        return false;
+    }
+  };
+
+  let validated_attestation_doc = match validate_attestation_doc_in_cert(&parsed_cert) {
+    Ok(attestation_doc) => attestation_doc,
+    Err(e) => {
+      eprintln!("An error occurred while validating the connection to this Cage: {e}");
+      return false;
+    }
+  };
+
+  let mut result = Ok(true);
+  for expected_pcrs in expected_pcrs_slice {
+      match validate_expected_pcrs(&validated_attestation_doc, &expected_pcrs) {
+          Ok(_) => return true,
+          Err(err) => result = Err(err),
+      }
+  }
+
+  match result {
+      Ok(_) => true,
+      Err(e) => {
+          eprintln!("Failed to validate that PCRs are as expected: {e}");
+          false
+      }
+  }
+}
