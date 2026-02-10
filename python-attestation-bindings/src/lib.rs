e83efd0cf2a6d9051c1cebeb7744187e621f07da
Liam Farrelly <github@l3f7.dev>
2026-02-10 14:20:38 +0000

    Upgrade pyo3 (#155)
    * chore: bump pyo3 dependency
    
    * chore: update getters and str impls to use macros
    
    * chore: declare pytest dep, bump pyo3 cross version
    
    * chore: add test for getters, update package description

--- python-attestation-bindings/src/lib.rs
+++ python-attestation-bindings/src/lib.rs
@@ -2,16 +2,19 @@ use attestation_doc_validation::{
     attestation_doc::{validate_expected_pcrs, PCRProvider},
     parse_cert, validate_attestation_doc_against_cert, validate_attestation_doc_in_cert,
 };
-use pyo3::exceptions::PyValueError;
-use pyo3::prelude::*;
+use pyo3::{exceptions::PyValueError, prelude::*};
 
-#[pyclass]
-#[derive(Debug, PartialEq, Eq, Clone, Default)]
+#[pyclass(str)]
+#[derive(Debug, PartialEq, Eq, FromPyObject, Default)]
 
 pub struct PCRs {
+    #[pyo3(get)]
     pcr_0: Option<String>,
+    #[pyo3(get)]
     pcr_1: Option<String>,
+    #[pyo3(get)]
     pcr_2: Option<String>,
+    #[pyo3(get)]
     pcr_8: Option<String>,
 }
 
@@ -47,25 +50,31 @@ impl PCRs {
         }
     }
 
-    fn __contains__<'py>(&self, py: Python<'py>, key: PyObject) -> PyResult<bool> {
-        let lookup_key = key.extract::<String>(py)?.to_lowercase();
+    fn __contains__<'py>(&self, key: &Bound<'py, PyAny>) -> PyResult<bool> {
+        let lookup_key = key.extract::<String>()?.to_lowercase();
         let matching_pcr = self.lookup_pcr(&lookup_key);
         Ok(matching_pcr.is_some())
     }
 
-    fn __getitem__<'py>(&self, py: Python<'py>, key: PyObject) -> PyResult<PyObject> {
-        let lookup_key = key.extract::<String>(py)?.to_lowercase();
-        let matching_pcr = self.lookup_pcr(&lookup_key);
-        let pcr_object = matching_pcr.map(String::from).to_object(py);
-        Ok(pcr_object)
-    }
+    fn __getitem__<'py>(&self, key: &Bound<'py, PyAny>) -> PyResult<Option<String>> {
+        let lookup_key = key.extract::<String>()?.to_lowercase();
+        let Some(matching_pcr) = self.lookup_pcr(&lookup_key) else {
+            return Ok(None);
+        };
 
-    fn __str__(&self) -> String {
-        self.to_string()
+        let pcr_object = String::from(matching_pcr);
+        Ok(Some(pcr_object))
     }
 
     fn __repr__(&self) -> String {
-        self.to_string()
+        PCRProvider::to_string(self)
+    }
+}
+
+impl std::fmt::Display for PCRs {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let pcr_str = PCRProvider::to_string(self);
+        f.write_str(&pcr_str)
     }
 }
 
@@ -172,10 +181,13 @@ pub fn attest_enclave(
 
 /// A small python module offering bindings to the rust attestation doc validation project
 #[pymodule]
-fn evervault_attestation_bindings(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(attest_connection, m)?)?;
-    m.add_function(wrap_pyfunction!(attest_cage, m)?)?;
-    m.add_function(wrap_pyfunction!(attest_enclave, m)?)?;
-    m.add_class::<PCRs>()?;
-    Ok(())
+mod evervault_attestation_bindings {
+    #[pymodule_export]
+    use super::attest_connection;
+    #[pymodule_export]
+    use super::attest_cage;
+    #[pymodule_export]
+    use super::attest_enclave;
+    #[pymodule_export]
+    use super::PCRs;
 }
